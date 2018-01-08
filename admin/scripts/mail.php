2dbb1640f9f23ba9f0be8845777fa15652dc1991
sydneybucc <s_bucci@fanshaweonline.ca>
2018-01-08 15:59:20 -0500

    testing contact page

---
+++ admin/scripts/mail.php
@@ -0,0 +1,22 @@
+<?php
+  //echo "From mail file.";
+
+  function redirect_to($location){
+    if($location != NULL){
+      header("Location: {$location}");
+      exit;
+    }
+  }
+
+  //make sure that the list is in the same order as it is called. DO NOT mix up the order!!
+  function submitMessage($direct, $name, $email, $message) {
+    //echo "From submit message()";
+    $to = "sydneybucc@gmail.com";
+    $subject = "message from user from hosting";
+    $extra = "Reply to: ".$email;
+    $msg = "Name: ".$name."\n\nEmail: ".$email."\n\nMessage: ".$message;
+    mail($to,$subject,$msg,$extra);
+    //$direct = $direct."?name={$name}";
+  }
+
+ ?>
