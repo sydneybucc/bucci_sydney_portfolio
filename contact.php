3118c6a98e65b6f452ca21ef67ab9a203f4839a0
sydneybucc <s_bucci@fanshaweonline.ca>
2018-01-08 17:58:10 -0500

    contact form should be working

--- contact.php
+++ contact.php
@@ -1,17 +1,25 @@
 <?php
   require_once ('admin/scripts/config.php');
-  if (isset($_POST['name'])){
+  if (isset($_POST['fullName'])){
+    
     //echo "yup";
+    
     $direct = "thankyou.php";
-    $name = $_POST['name'];
+    $name = $_POST['fullName'];
     $email = $_POST['email'];
     $subject = $_POST['subject'];
     $message = $_POST['message'];
+    
     //echo $name;
+    
     if($subject === "") {
       echo "send mail";
       $sendMail = submitMessage($direct, $name, $email, $message);
     }//else{
-      //Go away bot!";
+      //echo "Go away bot!";
     //}
+  }
+
+  //echo '<pre>' . var_dump($_POST['message']) . '</pre>';
+  //echo '<pre>' . var_dump($_POST) . '</pre>';
 ?>
