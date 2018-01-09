4102165de6b8448931cc06c517bac49fe09f4150
sydneybucc <s_bucci@fanshaweonline.ca>
2018-01-09 17:18:50 -0500

    thank you page works

--- contact.php
+++ contact.php
@@ -23,3 +23,29 @@
   //echo '<pre>' . var_dump($_POST['message']) . '</pre>';
   //echo '<pre>' . var_dump($_POST) . '</pre>';
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Thank You</title>
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.typekit.net/wta7myy.css">
+  <link rel="stylesheet" href="css/foundation.css">
+  <link href="css/reset.css" rel="stylesheet" type="text/css" media="screen">
+  <link href="css/main.css" rel="stylesheet" type="text/css" media="screen">
+</head>
+<body>
+
+<section>
+  <h2>Thank you</h2>
+  <h3>I have recieved your email and I will get back to you within 24 hours.</h3>
+</section>
+<section>
+  <a class="backButton" href="index.html">BACK</a>
+</section>
+  
+</body>
+</html>
