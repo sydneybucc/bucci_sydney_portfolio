9604e8a169becbbd21117ff206fff61452fa2a9e
sydneybucc <s_bucci@fanshaweonline.ca>
2018-01-09 02:22:26 -0500

    attempted lightbox, failed. try again later

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 (function() {
 	"use strict";
 	console.log("SEAF Fired");
-	
+    
+    ///////////////////////
+    //Main Navagation Scroll
+
 	var nav = document.querySelector("#mainNav");
 	var links = nav.querySelectorAll("a");
-	console.log(links);
+	//console.log(links);
 	
 	function scrollit(evt) { //use evt when you need to grab the id or something stored in it
-		console.log("From scrollit");
+		//console.log("From scrollit");
         var idNum = evt.currentTarget.id;
         //console.log(evt.currentTarget.id);
 		TweenLite.to(window, 1, {scrollTo:{y:"#section"+idNum, offsetY:20}});
@@ -16,5 +19,45 @@
 	for(var i=0; i<links.length; i++) {
 		links[i].addEventListener("click", scrollit, false);
 	}
-	
+    
+    //////////
+    //Lightbox
+
+    var moreButtons = document.querySelectorAll('.more');
+
+    function lightBoxOpen() {
+        moreButtons.addEventListener('click', openlightBox, false);
+        console.log("From lightBoxOpen");
+    }
+
+    function openlightBox(currentIndex, currentObject) {
+        //console.log("From openLightbox");
+        //debugger;
+        //window.scrollTo(0, 0);
+        //document.body.style.overflow = "hidden";
+
+        let lightBox = document.querySelector('.lightbox');
+        lightbox.style.display = 'block';
+
+        let lightboxImg = lightbox.querySelector('img');
+        let lightboxClose = lightbox.querySelector('close-lightbox');
+        let lightboxDesc = lightbox.querySelector('p');
+
+        lightboxImg.src = "images/" + currentObject.images[currentIndex];
+        lightboxDesc.innerHTML = currentObject.description[currentIndex];
+
+        lightboxClose.addEventListener('click', closeLightbox, false)
+    }
+
+    function closeLightbox() {
+        document.body.style.overflow = "auto";
+
+        let lightbox = document.querySelector('.lightbox');
+        let lightboxImg = lightbox.querySelector('.img');
+        let lightboxDesc = lightbox.querySelector('p');
+        
+        lightbox.style.display = "";
+        lightboxImg.src = "";
+        lightboxDesc.innerHTML = "";
+    }
 })();
