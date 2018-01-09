6c2fa60f5fddcc888685c9ad74b8682787bed9cc
sydneybucc <s_bucci@fanshaweonline.ca>
2018-01-08 21:29:49 -0500

    adding tweenmax to js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
-// JavaScript Document
-// Single Comment
-
-/*
-1.
-2.
-3.
-4.
-5.
-*/
-
-// Variables
-
-// Functions
-
-// Listeners
+(function() {
+	"use strict";
+	console.log("SEAF Fired");
+	
+	var nav = document.querySelector("#mainNav");
+	var links = nav.querySelectorAll("a");
+	console.log(links);
+	
+	function scrollit(evt) { //use evt when you need to grab the id or something stored in it
+		//console.log("From scrollit");
+		//console.log(evt.currentTarget.id);
+		var idNum = evt.currentTarget.id;
+		TweenLite.to(window, 1, {scrollTo:{y:"#section"+idNum, offsetY:20}});
+	}
+	
+	for(var i=0; i<links.length; i++) {
+		links[i].addEventListener("click", scrollit, false);
+	}
+	
+})();
